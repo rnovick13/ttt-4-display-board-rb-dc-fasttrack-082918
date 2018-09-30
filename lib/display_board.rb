9652855d067ef1f7d0f3cfd0b7f92f0aa44b99e6
Rachel <rnovick13@gmail.com>
2018-09-30 02:35:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,32 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 # all Os
-def display_board
-  puts " O | O | O "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " O | O | O "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " O | O | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-display_board
-
-#blank
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-display_board
-
-# o in top left
-def display_board
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-display_board
